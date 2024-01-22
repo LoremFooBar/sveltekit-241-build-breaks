624bc6933c3b3c37a019240fd65f3c84016417f0
LoremFooBar <loremfoobardev@gmail.com>
2024-01-22 15:02:12 +0200

    add top level await and pin sveltekit to 2.4.1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script context="module">
+    const promise = new Promise(resolve => setTimeout(resolve, 20));
+    await promise;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
